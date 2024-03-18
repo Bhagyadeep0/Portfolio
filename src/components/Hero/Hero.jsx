4c07a69035bb75186da7b6c3d8c34e68d3e474f3
Bhagyadeep <b.ghosh0330@gmail.com>
2024-03-18 15:06:22 +0530

    Hero Created

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,15 +1,19 @@
 import "./hero.scss";
 
-import {motion} from "framer-motion"
+import {  easeInOut, motion } from "framer-motion";
 
 const Hero = () => {
   return (
-      
     <div className="hero">
       <div className="textContainer">
         <h2>Hello!</h2>
-        <h1>I am Web Developer and Graphic Designer</h1>
-        <motion.button whileHover={{borderRadius:"20px 0 0 20px"}}><b>Explore</b> {"->"} </motion.button>
+        <h1>I am a <motion.b whileHover={{color:"purple"}} transition={{ease:easeInOut}}>Web Developer</motion.b> and <motion.b whileHover={{color:"green"}} transition={{ease:easeInOut}}>Graphic Designer</motion.b></h1>
+        <motion.button whileHover={{ borderRadius: "20px 0 0 20px" }}>
+          <b>Resume</b>
+        </motion.button>
+        <motion.button whileHover={{ borderRadius: "20px 0 0 20px" }}>
+          <b>Contact</b>
+        </motion.button>
       </div>
       <div className="imgContainer">
         <img src="/zoro1.png" alt="zoro" />
